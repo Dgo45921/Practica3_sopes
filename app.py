fb79e45e447fc70a474d9d19b87926720427896c
Dgo45921 <3023135080101@ingenieria.usac.edu.gt>
2023-12-26 19:42:09 -0600

    zzz

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'imbilelou'
+app.config['MYSQL_DATABASE_DB'] = 'users'
+app.config['MYSQL_DATABASE_HOST'] = '34.170.111.220'
+mysql.init_app(app)
 
 
 @app.route('/')
@@ -13,11 +22,29 @@ def login():
     username = request.form['username']
     password = request.form['password']
 
-    # You can add your authentication logic here
-    # For simplicity, let's just print the credentials for now
-    print(f'Username: {username}, Password: {password}')
+    # Check if the user exists in the database
+    try:
+        # Create MySQL connection and cursor
+        conn = mysql.connect()
+        cursor = conn.cursor()
 
-    return 'Login successful!'
+        # Check if the user exists in the database
+        query = "SELECT * FROM credentials WHERE username = %s AND password = %s"
+        cursor.execute(query, (username, password))
+        result = cursor.fetchone()
+
+        # Close the cursor and connection
+        cursor.close()
+        conn.close()
+
+        # Return the result
+        if result:
+            return 'Login successful!'
+        else:
+            return 'User not found. Please check your credentials.'
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 
 if __name__ == '__main__':
